0651df5f37546e39e838fd36153d59dd4855ceb1
Halil Calik <halicalix@gmail.com>
2024-11-24 20:15:44 +0300

    Update Project25.cpp

--- Project25.cpp
+++ Project25.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
+	
  int sayilar[5];
  cout << "5 Tane Tamsayi Girmelisiniz!" << endl;
+ 
  // girilen degerleri bir int dizisinde sakla
+ 
  for (int i = 0; i < 5; ++i) {
+	 
  cout << i + 1 << ". Sayi giriniz..:";
+ 
  cin >> sayilar[i];
  
  }
