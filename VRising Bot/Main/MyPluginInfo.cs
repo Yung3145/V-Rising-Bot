8cae3eb19af5cc121eb917a27b484d04ab007c67
Halil Calik <halicalix@gmail.com>
2025-01-06 01:47:33 +0300

    Create MyPluginInfo.cs

---
+++ VRising Bot/Main/MyPluginInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace VRisingESP
+{
+	// Token: 0x02000009 RID: 9
+	[NullableContext(1)]
+	[Nullable(0)]
+	public static class MyPluginInfo
+	{
+		// Token: 0x04000028 RID: 40
+		public const string PLUGIN_GUID = "ESP";
+
+		// Token: 0x04000029 RID: 41
+		public const string PLUGIN_NAME = "ESP";
+
+		// Token: 0x0400002A RID: 42
+		public const string PLUGIN_VERSION = "1.0.0";
+	}
+}
