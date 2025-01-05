d0f6e46ea84da56c826f525e947bd222db5ba433
Halil Calik <halicalix@gmail.com>
2025-01-06 01:47:31 +0300

    Create Enums.cs

---
+++ VRising Bot/Main/Enums.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace VRisingESP.Enums
+{
+	// Token: 0x02000010 RID: 16
+	public enum Colors
+	{
+		// Token: 0x04000038 RID: 56
+		Red,
+		// Token: 0x04000039 RID: 57
+		Green,
+		// Token: 0x0400003A RID: 58
+		Blue,
+		// Token: 0x0400003B RID: 59
+		Yellow,
+		// Token: 0x0400003C RID: 60
+		Cyan,
+		// Token: 0x0400003D RID: 61
+		Magenta,
+		// Token: 0x0400003E RID: 62
+		White,
+		// Token: 0x0400003F RID: 63
+		Black,
+		// Token: 0x04000040 RID: 64
+		Gray,
+		// Token: 0x04000041 RID: 65
+		LightBlue,
+		// Token: 0x04000042 RID: 66
+		DarkBlue,
+		// Token: 0x04000043 RID: 67
+		LightGreen,
+		// Token: 0x04000044 RID: 68
+		DarkGreen,
+		// Token: 0x04000045 RID: 69
+		LightYellow,
+		// Token: 0x04000046 RID: 70
+		DarkYellow,
+		// Token: 0x04000047 RID: 71
+		Orange,
+		// Token: 0x04000048 RID: 72
+		Purple,
+		// Token: 0x04000049 RID: 73
+		Brown,
+		// Token: 0x0400004A RID: 74
+		Pink,
+		// Token: 0x0400004B RID: 75
+		Teal
+	}
+}
