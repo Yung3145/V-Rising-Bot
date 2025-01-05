7f1226f220282e8ba5af43ca3b8e3f30558e2788
Halil Calik <halicalix@gmail.com>
2025-01-06 01:58:05 +0300

    Create Mappy.Entity.cs

---
+++ VRising Bot/Editable Area (-)/Mappy.Entity.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+namespace Mappy
+{
+	// Token: 0x02000018 RID: 24
+	public class Entity
+	{
+		// Token: 0x17000026 RID: 38
+		// (get) Token: 0x0600008B RID: 139 RVA: 0x00002611 File Offset: 0x00000811
+		// (set) Token: 0x0600008C RID: 140 RVA: 0x00002619 File Offset: 0x00000819
+		public int BloodQuality { get; set; }
+
+		// Token: 0x17000027 RID: 39
+		// (get) Token: 0x0600008D RID: 141 RVA: 0x00002622 File Offset: 0x00000822
+		// (set) Token: 0x0600008E RID: 142 RVA: 0x0000262A File Offset: 0x0000082A
+		public Vector3 Position { get; set; }
+	}
+}
