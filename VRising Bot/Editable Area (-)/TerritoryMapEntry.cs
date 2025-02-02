485ef052f9072e741b4d3fb71a4c1b4c48e6e5c0
Leviathan <132717919+ca1ik@users.noreply.github.com>
2025-02-02 23:23:51 +0300

    Update TerritoryMapEntry.cs

--- VRising Bot/Editable Area (-)/TerritoryMapEntry.cs
+++ VRising Bot/Editable Area (-)/TerritoryMapEntry.cs
@@ -8,21 +8,4 @@ public class TerritoryMapEntry
 	// Token: 0x06000071 RID: 113 RVA: 0x0000252D File Offset: 0x0000072D
 	public void SetData(bool minimap, int globalTerritoryIndex, TerritoryMapEntry.LocalTerritoryTeamStatus teamStatus, float zoomFactor, bool hovered, Texture2D revealTexture, Vector2 centerPosWS, float width, float height)
 	{
-		Debug.Log(string.Format("SetData çağrıldı. TeamStatus: {0}, Blood Yüksek!", teamStatus));
-	}
-
-	// Token: 0x02000012 RID: 18
-	public enum LocalTerritoryTeamStatus
-	{
-		// Token: 0x0400004D RID: 77
-		Neutral,
-		// Token: 0x0400004E RID: 78
-		Enemy,
-		// Token: 0x0400004F RID: 79
-		Ally,
-		// Token: 0x04000050 RID: 80
-		AllyDecay,
-		// Token: 0x04000051 RID: 81
-		EnemyDecay
-	}
-}
+		Debug.Log(string.Format("SetData çağrıldı. TeamStatus: {0}, 
