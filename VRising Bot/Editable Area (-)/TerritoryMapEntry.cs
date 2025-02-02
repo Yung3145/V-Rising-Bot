7e02b78307f062373eedc46c7ace27d36e83f8b2
Leviathan <132717919+ca1ik@users.noreply.github.com>
2025-02-02 23:24:08 +0300

    Update TerritoryMapEntry.cs

--- VRising Bot/Editable Area (-)/TerritoryMapEntry.cs
+++ VRising Bot/Editable Area (-)/TerritoryMapEntry.cs
@@ -5,7 +5,4 @@ using ProjectM; // Can use plt library for analysis
 // Token: 0x02000011 RID: 17
 public class TerritoryMapEntry
 {
-	// Token: 0x06000071 RID: 113 RVA: 0x0000252D File Offset: 0x0000072D
-	public void SetData(bool minimap, int globalTerritoryIndex, TerritoryMapEntry.LocalTerritoryTeamStatus teamStatus, float zoomFactor, bool hovered, Texture2D revealTexture, Vector2 centerPosWS, float width, float height)
-	{
-		Debug.Log(string.Format("SetData çağrıldı. TeamStatus: {0}, 
+	// Token: 0x06000071 RID: 113 çağrıldı. TeamStatus: {0}, 
